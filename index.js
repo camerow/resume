e6729443af50edfd990716f926119d5a77d6aa87
Will Cameron <cameron.will@gmail.com>
2016-05-16 10:49:46 -0700

    postinstall add

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const Inert = require('inert');
 const fs = require('fs');
 
 const server = new Hapi.Server({
-  connections: {
-    routes: {
-      files: {
-        relativeTo: Path.resolve(Path.join(__dirname, 'build'))
-      }
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.resolve(Path.join(__dirname, 'build'))
+            }
+        }
     }
-  }
 });
 
-server.connection({ port: process.env.PORT || 3000 });
+server.connection({
+    port: process.env.PORT || 3000
+});
 
 server.register(Inert, () => {});
 
@@ -29,29 +31,27 @@ server.register(Inert, () => {});
 //   }
 // });
 server.route([{
-        method: 'GET',
-        path: '/404',
-        handler: function (request, reply) {
-
-            reply("Will's sad error page.");
-        }
-    }, {
-        method: 'GET',
-        path: '/{param*}',
-        handler: {
-            directory: {
-                path: '.',
-                redirectToSlash: true,
-                index: true
-            }
+    method: 'GET',
+    path: '/404',
+    handler: function(request, reply) {
+        reply(request.headers);
+    }
+}, {
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: '.',
+            redirectToSlash: true,
+            index: true
         }
     }
-]);
+}]);
 
-server.ext('onPreResponse', function (request, reply) {
+server.ext('onPreResponse', function(request, reply) {
 
     if (request.response.isBoom) {
-      console.log("Resp:", request.response);
+        console.log("Resp:", request.response);
         // Inspect the response here, perhaps see if it's a 404?
         return reply.redirect('/404');
     }
@@ -60,10 +60,10 @@ server.ext('onPreResponse', function (request, reply) {
 });
 
 server.start((err) => {
-  if (err) {
-    console.error("ERROR: ", err);
-    throw err;
-  }
+    if (err) {
+        console.error("ERROR: ", err);
+        throw err;
+    }
 
-  console.log('Server running at:', server.info.uri);
+    console.log('Server running at:', server.info.uri);
 });
