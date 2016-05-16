aab94004ce5502a89d1ad7fd959ff19b03e0f263
Will Cameron <cameron.will@gmail.com>
2016-05-15 20:44:00 -0700

    test

--- index.js
+++ index.js
@@ -15,18 +15,45 @@ server.connection({ port: process.env.PORT || 3000 });
 
 server.register(Inert, () => {});
 
-server.route({
-  method: 'GET',
-  path: '/{param*}',
-  handler: {
-    directory: {
-        path: '.',
-        redirectToSlash: true,
-        index: true
-    }
-  }
-});
+// server.route({
+//   method: 'GET',
+//   path: '/{param*}',
+//   handler: {
+//     directory: {
+//         path: '.',
+//         redirectToSlash: true,
+//         index: true
+//     }
+//   }
+// });
+server.route([{
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
 
+            reply('Welcome home!');
+        }
+    }, {
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: '.',
+                redirectToSlash: false,
+                index: true
+            }
+        }
+    }
+]);
+server.ext('onPreResponse', function (request, reply) {
+
+    if (request.response.isBoom) {
+        // Inspect the response here, perhaps see if it's a 404?
+        return reply.redirect('/');
+    }
+
+    return reply.continue();
+});
 server.start((err) => {
   if (err) {
     throw err;
