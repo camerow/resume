ef8e760e20ae09a5f78a5999e7a823cb2f71cda8
Will Cameron <cameron.will@gmail.com>
2016-05-17 22:38:00 -0700

    spacing

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const Inert = require('inert');
 const fs = require('fs');
 
 const server = new Hapi.Server({
-    connections: {
-        routes: {
-            files: {
-                relativeTo: Path.resolve(Path.join(__dirname, 'build'))
-            }
-        }
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.resolve(Path.join(__dirname, 'build'))
+      }
     }
+  }
 });
 
 server.connection({
-    port: process.env.PORT || 3000
+  port: process.env.PORT || 3000
 });
 
 server.register(Inert, () => {});
@@ -31,39 +31,39 @@ server.register(Inert, () => {});
 //   }
 // });
 server.route([{
-    method: 'GET',
-    path: '/404',
-    handler: function(request, reply) {
-        reply(request.headers);
-    }
+  method: 'GET',
+  path: '/404',
+  handler: function(request, reply) {
+    reply(request.headers);
+  }
 }, {
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-        directory: {
-            path: '.',
-            redirectToSlash: true,
-            index: true
-        }
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: '.',
+      redirectToSlash: true,
+      index: true
     }
+  }
 }]);
 
 server.ext('onPreResponse', function(request, reply) {
 
-    if (request.response.isBoom) {
-        console.log("Resp:", request.response);
-        // Inspect the response here, perhaps see if it's a 404?
-        return reply.redirect('/404');
-    }
+  if (request.response.isBoom) {
+    console.log("Resp:", request.response);
+    // Inspect the response here, perhaps see if it's a 404?
+    return reply.redirect('/404');
+  }
 
-    return reply.continue();
+  return reply.continue();
 });
 
 server.start((err) => {
-    if (err) {
-        console.error("ERROR: ", err);
-        throw err;
-    }
+  if (err) {
+    console.error("ERROR: ", err);
+    throw err;
+  }
 
-    console.log('Server running at:', server.info.uri);
+  console.log('Server running at:', server.info.uri);
 });
